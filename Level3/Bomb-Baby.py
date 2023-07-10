392e216f4630fcb5ed9d5bdcdfff7df7cd5f03ac
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2023-07-11 00:18:36 +0530

    Update Bomb-Baby.py

--- Level3/Bomb-Baby.py
+++ Level3/Bomb-Baby.py
@@ -1,1 +1,33 @@
+def multiplier(a, b):
+    diff = a - b
+    return (diff / b) + 1
+
+def solution(x, y):
+    step, m, f = 0, int(x), int(y)
+    while True:
+        if m <= 0 or f <= 0:
+            break
+        if m > 100 or f > 100:
+            if m > f:
+                mul = multiplier(m, f)
+                m -= f * mul
+                step += mul
+            elif f > m:
+                mul = multiplier(f, m)
+                f -= m * mul
+                step += mul
+            else:
+                break
+        else:
+            if m > f:
+                m -= f
+            elif f > m:
+                f -= m
+            else:
+                break
+            step += 1
+    
+    if m == 1 and f == 1 and step >= 0:
+        return str(step)
+    return 'impossible'
 
