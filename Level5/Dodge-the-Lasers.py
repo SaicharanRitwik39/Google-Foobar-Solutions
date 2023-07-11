7302bb85e855e8d55ac8b3f13d1a15eb9be78387
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2023-07-11 09:55:34 +0530

    Update Dodge-the-Lasers.py

--- Level5/Dodge-the-Lasers.py
+++ Level5/Dodge-the-Lasers.py
@@ -1,1 +1,12 @@
+from decimal import *
 
+def sum_floor(a, n):
+    if n == 0:
+        return 0
+    np = int((a - 1) * n)
+    return n * np + n * (n + 1) / 2 - np * (np + 1) / 2 - sum_floor(a, np)
+
+def solution(s):
+    getcontext().prec = 101
+    sqrt2 = Decimal(2).sqrt()
+    return str(int(sum_floor(sqrt2, int(s))))
