97df0ce5304ceb21632572c2f014347436949cc3
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2024-07-07 11:57:28 +0530

    Update Skipping-Work-Answer.py

--- Level1/Skipping-Work-Answer.py
+++ Level1/Skipping-Work-Answer.py
@@ -1,12 +1,7 @@
 def solution(x, y):
     xor_result = 0
-
-    # Perform XOR operation on all elements in list x
     for num in x:
         xor_result ^= num
-
-    # Perform XOR operation on all elements in list y
     for num in y:
         xor_result ^= num
-
     return xor_result
